469f0313a8583a67663bd5331ff67202238f1963
SuperTikuwa <tikuwamk2@gmail.com>
2021-10-03 22:15:54 +0900

    hoge

---
+++ src/js/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <img className="profile_image" src="https://github.com/SuperTikuwa.png" />
+      <h2 className="profile_name">Super Tikuwa</h2>
+      <p className="profile_bio">
+        GoとReactがちょっとできるサーバーサイドエンジニアです．
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
