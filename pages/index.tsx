0cd6b91f9a7ed22a2a3ce1e07261635f0b9a25b5
SuperTikuwa <tikuwamk2@gmail.com>
2022-11-17 17:47:22 +0900

    hoge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import App from './app';
 
 const Index = () => {
   return (
     <>
+      <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4774589443305457" crossOrigin="anonymous"></script>
+      </Head>
       <App />
     </>
   );
